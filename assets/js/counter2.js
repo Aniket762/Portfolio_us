3d19933fbd34b4e5abdccff6679b30a75f68e816
Sumana Basu <63084088+sumana2001@users.noreply.github.com>
2020-10-01 00:47:24 +0530

    Counter javascript added

---
+++ assets/js/counter2.js
@@ -0,0 +1,48 @@
+$(function() {
+
+   
+
+    /* Counters  */
+    if ($(".section-counters .start").length>0) {
+        $(".section-counters .start").each(function() {
+            var stat_item = $(this),
+            offset = stat_item.offset().top;
+            $(window).scroll(function() {
+                if($(window).scrollTop() > (offset - 1000) && !(stat_item.hasClass('counting'))) {
+                    stat_item.addClass('counting');
+                    stat_item.countTo();
+                }
+            });
+        });
+    };
+
+	// another custom callback for counting to infinity
+	$('#infinity').data('countToOptions', {
+		onComplete: function (value) {
+		  count.call(this, {
+		    from: value,
+		    to: value + 1
+		  });
+		}
+	});
+
+	$('#infinity').each(count);
+
+	function count(options) {
+        var $this = $(this);
+        options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+        $this.countTo(options);
+    }
+
+    // Navigation overlay
+    var s = skrollr.init({
+            forceHeight: false,
+            smoothScrolling: false,
+            mobileDeceleration: 0.004,
+            mobileCheck: function() {
+                //hack - forces mobile version to be off
+                return false;
+            }
+    });
+    
+});
